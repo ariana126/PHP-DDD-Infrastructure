042f0c3802aabfa11409942476814da968b14293
Ariana Maghsoudi <ariana.maghsoudi82@gmail.com>
2025-08-11 02:20:14 +0330

    feat: Add Symfony domain event dispatcher

---
+++ src/Infrastructure/Symfony/DomainEventDispatcher.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Symfony;
+
+use DDD\Domain\AggregateRoot;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+/*
+ * Be aware that, by default, the Symfony event dispatcher works synchronously.
+ * As a result, if you use this implementation and the Doctrine base repository class written in this package,
+ *  an exception in one listener might cause the whole transaction to be rolled back.
+ */
+class DomainEventDispatcher implements \DDD\Domain\Service\DomainEventDispatcher
+{
+    /**
+     * @var EventDispatcherInterface
+     */
+    private EventDispatcherInterface $bus;
+
+    /**
+     * @param EventDispatcherInterface $bus
+     */
+    public function __construct(EventDispatcherInterface $bus)
+    {
+        $this->bus = $bus;
+    }
+
+    /**
+     * @param AggregateRoot $aggregateRoot
+     * @return void
+     */
+    public function dispatchAggregateRootEvents(AggregateRoot $aggregateRoot): void
+    {
+        foreach ($aggregateRoot->releaseEvents() as $domainEvent) {
+            $this->bus->dispatch($domainEvent);
+        }
+    }
+}
